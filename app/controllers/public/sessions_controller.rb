bd6a9861828ac0f6772a8d7d8ba1e0f27ef5fc02
“sakisakiko” <108504337+sakisakiko@users.noreply.github.com@users.noreply.github.com>
2022-10-16 04:57:16 +0000

    add code on public_session_controller

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -18,7 +18,20 @@ class Public::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # protected
+  protected
+
+  def customer_status
+    @customer=Customer.find_by(email:params[:customer][:email])
+    return if !@customer
+
+    if @customer.valid_password?(params[:customer][:password])&&(@customer.is_deleted=="2")
+      flash[:notice]="既に退会済みです。再度登録してご利用下さい。"
+    elsif @customer.valid_password?(params[:customer][:password])&&(@customer.is_deleted=="3")
+      flash[:notice]="このアカウントは現在利用停止されています。お問合せフォームからご連絡下さい。"
+    else
+    end
+
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params

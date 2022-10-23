e403db0de9a3b1fea1dcad87f9f1aaad6c31a01a
“sakisakiko” <108504337+sakisakiko@users.noreply.github.com@users.noreply.github.com>
2022-10-23 08:38:27 +0000

    fixed reference to integer

--- db/migrate/20221005064949_create_post_tags.rb
+++ db/migrate/20221005064949_create_post_tags.rb
@@ -1,8 +1,8 @@
 class CreatePostTags < ActiveRecord::Migration[6.1]
   def change
     create_table :post_tags do |t|
-      t.references :post_item, foreign_key: true
-      t.references :tag, foreign_key: true
+      t.integer :post_item_id, foreign_key: true
+      t.integer :tag_id, foreign_key: true
       t.timestamps
     end
     # 同じタグを２回保存するのは出来ないようになる(重複をさける)

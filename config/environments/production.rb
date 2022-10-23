df5ce8fbb5526762d97753a1af0c803d0d2a1dcc
“sakisakiko” <108504337+sakisakiko@users.noreply.github.com@users.noreply.github.com>
2022-10-23 11:25:54 +0000

    [Fix] production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,5 +117,4 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-  config.hosts << "f12917e6b928478899032e656d1df9c1.vfs.cloud9.us-east-1.amazonaws.com"
 end

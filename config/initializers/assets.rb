978fc331e4dd9587e580e4788b04a6677fb42479
“sakisakiko” <108504337+sakisakiko@users.noreply.github.com@users.noreply.github.com>
2022-11-07 07:54:50 +0000

    add layout_code for top_page

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,3 +12,13 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+
+
+# # scss個別読み込み設定
+# Rails.application.config.assets.precompile += %w(
+#   style.scss
+#   application.scss
+
+#   publics/*
+#   admins/*
+# )

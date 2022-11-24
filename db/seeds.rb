2b7004a1ca67406926699e7782e727e48117d4e5
“sakisakiko” <108504337+sakisakiko@users.noreply.github.com@users.noreply.github.com>
2022-11-24 11:52:13 +0000

    fixed seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,22 +11,22 @@
  end
 
 buy_prefectures = [
-  { id: 1, name: "北海道" }, { id: 2, name: "青森県" }, {id: 3, name: '岩手県'},
-  {id: 4, name: '宮城県'}, {id: 5, name: '秋田県'}, {id: 6, name: '山形県'},
-  {id: 7, name: '福島県'}, {id: 8, name: '茨城県'}, {id: 9, name: '栃木県'},
-  {id: 10, name: '群馬県'}, {id: 11, name: '埼玉県'}, {id: 12, name: '千葉県'},
-  {id: 13, name: '東京都'}, {id: 14, name: '神奈川県'}, {id: 15, name: '新潟県'},
-  {id: 16, name: '富山県'}, {id: 17, name: '石川県'}, {id: 18, name: '福井県'},
-  {id: 19, name: '山梨県'}, {id: 20, name: '長野県'}, {id: 21, name: '岐阜県'},
-  {id: 22, name: '静岡県'}, {id: 23, name: '愛知県'}, {id: 24, name: '三重県'},
-  {id: 25, name: '滋賀県'}, {id: 26, name: '京都府'}, {id: 27, name: '大阪府'},
-  {id: 28, name: '兵庫県'}, {id: 29, name: '奈良県'}, {id: 30, name: '和歌山県'},
-  {id: 31, name: '鳥取県'}, {id: 32, name: '島根県'}, {id: 33, name: '岡山県'},
-  {id: 34, name: '広島県'}, {id: 35, name: '山口県'}, {id: 36, name: '徳島県'},
-  {id: 37, name: '香川県'}, {id: 38, name: '愛媛県'}, {id: 39, name: '高知県'},
-  {id: 40, name: '福岡県'}, {id: 41, name: '佐賀県'}, {id: 42, name: '長崎県'},
-  {id: 43, name: '熊本県'}, {id: 44, name: '大分県'}, {id: 45, name: '宮崎県'},
-  {id: 46, name: '鹿児島県'}, {id: 47, name: '沖縄県'}
+  { id: 1, name: "北海道" }, { id: 2, name: "青森県" }, { id: 3, name: "岩手県" },
+  { id: 4, name: "宮城県" }, { id: 5, name: "秋田県" }, { id: 6, name: "山形県" },
+  { id: 7, name: "福島県" }, { id: 8, name: "茨城県" }, { id: 9, name: "栃木県" },
+  { id: 10, name: "群馬県" }, { id: 11, name: "埼玉県" }, { id: 12, name: "千葉県" },
+  { id: 13, name: "東京都" }, { id: 14, name: "神奈川県"}, { id: 15, name: "新潟県" },
+  { id: 16, name: "富山県" }, { id: 17, name: "石川県" }, { id: 18, name: "福井県" },
+  { id: 19, name: "山梨県" }, { id: 20, name: "長野県" }, { id: 21, name: "岐阜県" },
+  { id: 22, name: "静岡県" }, { id: 23, name: "愛知県" }, { id: 24, name: "三重県" },
+  { id: 25, name: "滋賀県" }, { id: 26, name: "京都府" }, { id: 27, name: "大阪府" },
+  { id: 28, name: "兵庫県" }, { id: 29, name: "奈良県" }, { id: 30, name: "和歌山県" },
+  { id: 31, name: "鳥取県" }, { id: 32, name: "島根県" }, { id: 33, name: "岡山県" },
+  { id: 34, name: "広島県" }, { id: 35, name: "山口県" }, { id: 36, name: "徳島県" },
+  { id: 37, name: "香川県" }, { id: 38, name: "愛媛県" }, { id: 39, name: "高知県" },
+  { id: 40, name: "福岡県" }, { id: 41, name: "佐賀県" }, { id: 42, name: "長崎県" },
+  { id: 43, name: "熊本県" }, { id: 44, name: "大分県" }, { id: 45, name: "宮崎県" },
+  { id: 46, name: "鹿児島県" }, { id: 47, name: "沖縄県" }
 ]
 
 buy_prefectures.each do |buy_prefecture|
@@ -179,11 +179,11 @@ post_items = [
   {
     customer: Customer.first,
     category: Category.first(2).last,
-    name: "星果庵　金平糖（ほうじ茶）",
+    name: "星果庵 金平糖（ほうじ茶）",
     review: "ほうじ茶の香ばしい香りがして美味しいです！",
     evaluation: 5,
     price: 594,
-    shop: "宇治園　心斎橋本店",
+    shop: "宇治園 心斎橋本店",
     buy_prefecture_id: 27,
     release: true,
     image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post-image1.jpg"), filename:"sample-post-image1.jpg")
@@ -191,11 +191,11 @@ post_items = [
   {
     customer: Customer.first(2).last,
     category: Category.first(2).last,
-    name: "【天明堂】鳳梨萬頭　１０個入",
+    name: "【天明堂】鳳梨萬頭 10個入",
     review: "パイナップルの味がしてすごく美味しかったです",
     evaluation: 5,
     price: 1458,
-    shop: "八丁堀本店地階 広島みやげ　心斎橋本店",
+    shop: "八丁堀本店地階 広島みやげ 心斎橋本店",
     buy_prefecture_id: 34,
     release: true,
     image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post-image2.jpg"), filename:"sample-post-image2.jpg")
@@ -231,7 +231,7 @@ post_items = [
     review: "色々な味付けの明太子がセットなのが良いです！ペースト上ですがご飯によく合います！",
     evaluation: 5,
     price: 1340,
-    shop: "博多阪急地下　辛子明太子売場",
+    shop: "博多阪急地下 辛子明太子売場",
     buy_prefecture_id: 40,
     release: true,
     image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post-image5.jpg"), filename:"sample-post-image5.jpg")
@@ -263,7 +263,7 @@ post_items = [
   {
     customer: Customer.first(5).last,
     category: Category.first(3).last,
-    name: "山廃純米酒　飛良泉（720ml)",
+    name: "山廃純米酒 飛良泉（720ml)",
     review: "大吟醸だとやや濃厚に感じますが、さらっとしていて飲みやすかったです。",
     evaluation: 5,
     price: 1760,
@@ -275,7 +275,7 @@ post_items = [
   {
     customer: Customer.first(5).last,
     category: Category.first(4).last,
-    name: "丸亀　組うちわ",
+    name: "丸亀組うちわ",
     review: "伝統工芸の丸亀うちわです。涼むのに使用するのも良いですが形と絵柄がお気に入りなので飾っています",
     evaluation: 5,
     price: 2178,
@@ -299,7 +299,7 @@ post_items = [
   {
     customer: Customer.first(5).last,
     category: Category.first(4).last,
-    name: "小樽硝子　花瓶",
+    name: "小樽硝子 花瓶",
     review: "透明感のある鮮やかな硝子がとても綺麗で気に入っています",
     evaluation: 5,
     price: 2750,

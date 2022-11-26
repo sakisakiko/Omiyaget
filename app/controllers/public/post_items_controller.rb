0d57b7a91f349943504c5740f9a602baf79457f2
“sakisakiko” <108504337+sakisakiko@users.noreply.github.com@users.noreply.github.com>
2022-11-26 09:28:05 +0000

    commentout access_permission

--- app/controllers/public/post_items_controller.rb
+++ app/controllers/public/post_items_controller.rb
@@ -1,6 +1,6 @@
 class Public::PostItemsController < ApplicationController
   #会員または管理者がログインしているか判断するメソッド（indexアクションは除く）
-  before_action :customer_or_admin_sign_in, except: [:index]
+  # before_action :customer_or_admin_sign_in, except: [:index]
 
   before_action :correct_custoemr, only: [:edit, :update]
   before_action :correct_custoemr_show, only: [:show]
@@ -139,11 +139,11 @@ class Public::PostItemsController < ApplicationController
   end
 
   # ログインしていないユーザー（会員）と管理者は一覧ページ以外のページにアクセスできない
-  def customer_or_admin_sign_in
-    unless customer_signed_in? or admin_signed_in?
-      redirect_to root_path
-    end
-  end
+  # def customer_or_admin_sign_in
+  #   unless customer_signed_in? or admin_signed_in?
+  #     redirect_to root_path
+  #   end
+  # end
 
 
 end
